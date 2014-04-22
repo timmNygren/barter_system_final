dfbd82ebdf664695f7a4602547c5181170551b63
Timm Nygren <metroid3prime@yahoo.com>
2014-04-21 19:19:39 -0600

    created new barter_system rails application skeleton

---
+++ barter_system/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BarterSystem::Application.config.secret_key_base = '4fbd75857daa03bbab36c030fb21a9cc082c70e8ea9c9c88a0402b6a19c833cdd80a1e782348b5a1d6355aed9ac5b667268b6a662c96ae9a2527adcdfd3e119d'
